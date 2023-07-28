ab37e19d6e0a6eeda5533518c4b1b0f8f6063afc
Anddrei V. Berreezhkov <berezhkov.mail@yandex.ru>
2023-07-29 00:38:41 +0300

    add news_category migration

---
+++ migrations/m230728_213535_create_category_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category}}`.
+ */
+class m230728_213535_create_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%category}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255),
+            'order' => $this->integer(11)
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%category}}');
+    }
+}
