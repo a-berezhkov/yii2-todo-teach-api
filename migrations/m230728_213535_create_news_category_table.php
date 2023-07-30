dd1ca1b37ec29ed5645aed8842ae57d66b3bbb31
Anddrei V. Berreezhkov <berezhkov.mail@yandex.ru>
2023-07-30 13:06:19 +0300

    FIX category migration

--- migrations/m230728_213535_create_news_category_table.php
+++ migrations/m230728_213535_create_news_category_table.php
@@ -5,14 +5,14 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%category}}`.
  */
-class m230728_213535_create_category_table extends Migration
+class m230728_213535_create_news_category_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->createTable('{{%category}}', [
+        $this->createTable('{{%news_category}}', [
             'id' => $this->primaryKey(),
             'name' => $this->string(255),
             'order' => $this->integer(11)
@@ -24,6 +24,6 @@ class m230728_213535_create_category_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropTable('{{%category}}');
+        $this->dropTable('{{%news_category}}');
     }
 }
